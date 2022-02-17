aea348c3b50190bfdec63549cc5b21b0973bb4f9
Cqmille <70196927+Cqmille@users.noreply.github.com>
2022-02-17 09:49:00 +0100

    update navbar

--- src/components/publiq-nav/publiq-nav.tsx
+++ src/components/publiq-nav/publiq-nav.tsx
@@ -38,28 +38,28 @@ export class PubliqNav {
             <header>
                 <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-primary d-flex flex-row">
                     <div class="container-fluid">
-                    <stencil-route-link url="/"><a class="navbar-brand abs" href="#">(Re)ssources Relationnelles</a></stencil-route-link>
+                    <stencil-route-link url="/" class="navbar-brand abs">(Re)ssources Relationnelles</stencil-route-link>
                         <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNavbar">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="navbar-collapse collapse" id="collapseNavbar">
                             <ul class="navbar-nav">
                                 <li class="nav-item active">
-                                <stencil-route-link url="/"><a class="nav-link" href="#">Accueil</a></stencil-route-link>
+                                <stencil-route-link url="/" class="nav-link">Accueil</stencil-route-link>
                                 </li>
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Catégories </a>
                                     <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarScrollingDropdown">
-                                        <li><stencil-route-link url="/tags-ressources/sante"><a class="dropdown-item" >Santé</a></stencil-route-link></li>
-                                        <li><stencil-route-link url="/tags-ressources/education"><a class="dropdown-item" >Éducation</a></stencil-route-link></li>
-                                        <li><stencil-route-link url="/tags-ressources/sport"><a class="dropdown-item" >Sports</a></stencil-route-link></li>
-                                        <li><stencil-route-link url="/tags-ressources/association"><a class="dropdown-item" >Associations</a></stencil-route-link></li>
-                                        <li><stencil-route-link url="/tags-ressources/emploi"><a class="dropdown-item" >Emploi</a></stencil-route-link></li>
-                                        <li><stencil-route-link url="/tags-ressources/senior"><a class="dropdown-item" >Sénior</a></stencil-route-link></li>
+                                        <li><stencil-route-link class="dropdown-item" url="/tags-ressources/sante"><p>Santé</p></stencil-route-link></li>
+                                        <li><stencil-route-link class="dropdown-item" url="/tags-ressources/education"><p>Éducation</p></stencil-route-link></li>
+                                        <li><stencil-route-link class="dropdown-item" url="/tags-ressources/sport"><p>Sports</p></stencil-route-link></li>
+                                        <li><stencil-route-link class="dropdown-item" url="/tags-ressources/association"><p>Associations</p></stencil-route-link></li>
+                                        <li><stencil-route-link class="dropdown-item" url="/tags-ressources/emploi"><p>Emploi</p></stencil-route-link></li>
+                                        <li><stencil-route-link class="dropdown-item" url="/tags-ressources/senior"><p>Sénior</p></stencil-route-link></li>
                                         <li>
                                             <hr class="dropdown-divider"></hr>
                                         </li>
-                                        <li><a class="dropdown-item" href="#">Au hasard</a></li>
+                                        <li><a class="dropdown-item">Au hasard</a></li>
                                     </ul>
                                 </li>
                                 <li class="nav-item active">
@@ -71,23 +71,19 @@ export class PubliqNav {
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> mon compte </a>
                                     <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarScrollingDropdown">
-                                        <li><stencil-route-link url="/monEspace"><a class="dropdown-item" >mes ressources</a></stencil-route-link></li>
-                                        <li><stencil-route-link url="/newRessource"><a class="dropdown-item" >créer ressource</a></stencil-route-link></li>
+                                        <li><stencil-route-link url="/monEspace" class="nav-link">mes ressources</stencil-route-link></li>
+                                        <li><stencil-route-link url="/newRessource" class="nav-link">créer ressource</stencil-route-link></li>
                                     </ul>
                                 </li>
                                 </ul>
 
                                 :<ul class="navbar-nav ms-auto">
-                                <stencil-route-link url="/connexion">
                                     <li class="nav-item">
-                                        <a class="nav-link" data-bs-target="#myModal" data-bs-toggle="modal">Connexion</a>
-                                    </li>
-                                </stencil-route-link>
-                                <stencil-route-link url="/inscription">				
+                                        <stencil-route-link url="/connexion" class="nav-link">Connexion</stencil-route-link>	
+                                    </li> 
                                     <li class="nav-item">
-                                        <a class="nav-link" data-bs-target="#myModal" data-bs-toggle="modal">Inscription</a>
+                                        <stencil-route-link url="/inscription" class="nav-link">Inscription</stencil-route-link>	
                                     </li>
-                                </stencil-route-link>	
                                 </ul>}
                         </div>
                     </div>
