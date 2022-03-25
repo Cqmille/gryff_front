7e5680f1df6df6e09a33a6e49c89f8b4fed1edaf
Cqmille <70196927+Cqmille@users.noreply.github.com>
2022-03-25 11:54:40 +0100

    update catégories ressources

--- src/components/publiq-ressource-tags/publiq-ressource-tags.tsx
+++ src/components/publiq-ressource-tags/publiq-ressource-tags.tsx
@@ -36,26 +36,31 @@ import { Ressources } from '../../utils/Ressources';
     }
     async redirect(event){
         this.history.push(`/afficherRessource/${event.target.value}`, {});   // Permet de charger une nouvelle page (ici c'est l'accueil car aucun)
-}
+    }
 
     render(){
         if(this.mesRessources){
             return (
                 <div class='container'>
-                    <h1 class='uppercase text-center'>{this.match.params.tags}</h1>
-                    <div class="row mx-3 mt-5">
-                        <div class="col-sm-2"></div>
-                        <div class="col-sm-8 ">
+                    <div class="d-flex justify-content-center">
+                        <p class="titre">{this.match.params.tags.charAt(0).toUpperCase() + this.match.params.tags.slice(1)}</p>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-sm-8">
                             {this.mesRessources.map((ressource : Ressources) =>
-                            <div class='border border-primary bg-light rounded mb-3 listshadow'>
-                                <div class='row mt-1'><div class='col-7 col-md-9 col-lg-10 '><h2 class='ms-3'>{ressource.titre}</h2></div><div class='col-2'>{ressource.prenomNomUser}</div></div>
-
-                                <div class='row ms-5 mt-1 mb-1 fs-5 '><div class='col-11'><p class='truncate'>{ressource.resume}</p></div></div>
-                            
-                                <div class='row mt-1'><div class='col-8 col-sm-3 col-md-7'><button class="btn btn-primary border text-light ms-3 mb-1" value={ressource._id} onClick={(event) => this.redirect(event)}>En savoir plus</button></div><div class='col-8 col-md-5'>Publié:{ressource.datePublication}</div></div>
+                            <div class='bloc-ressource p-2'>
+                                <div class="row d-flex align-items-baseline">
+                                    <div class="d-flex justify-content-between">
+                                        <h2 class=''>{ressource.titre}</h2>
+                                        <div class=''><button class="btn btn-primary text-light" value={ressource._id} onClick={(event) => this.redirect(event)}>Voir la ressource</button></div>
+                                    </div>
+                                    <div class=''><p>par {ressource.prenomNomUser}, le {ressource.datePublication.substring(0,10)}</p></div>
+                                </div>
+                                <div class=''><p class='truncate'>{ressource.resume}</p></div>
+                                <div class="row">
+                                </div>
                             </div>)}
                         </div>     
-                        <div class="col-sm-2"></div>
                     </div>
                 </div>
             )
